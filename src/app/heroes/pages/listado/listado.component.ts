994826508442409568a52dfebc2f055a8adb81c6
Julio de la Matta <julio.matta@berger-levrault.com>
2022-08-25 13:39:51 +0200

    Sección 14

--- src/app/heroes/pages/listado/listado.component.ts
+++ src/app/heroes/pages/listado/listado.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Heroe } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
-  styleUrls: []
+  styles: []
 })
 export class ListadoComponent implements OnInit {
 
-  constructor() { }
+  heroes: Heroe[] = [];
+
+  constructor(private heroesSevice: HeroesService) { }
 
   ngOnInit(): void {
+    this.heroesSevice.getHeroes().subscribe(resp => this.heroes = resp);
   }
 
 }
